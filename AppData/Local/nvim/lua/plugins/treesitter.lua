2572454ad7397a2d6546312135e61e4a9b7f4253
tscott12331 <t.scott12331@gmail.com>
2025-11-20 19:42:01 -0800

    Configure treesitter to use cl compiler and remove auto install

--- AppData/Local/nvim/lua/plugins/treesitter.lua
+++ AppData/Local/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,16 @@
+-- IMPORTANT
+-- cl compiler only works in x64 tools env, need to figure out why
 return {
     "nvim-treesitter/nvim-treesitter",
     build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
+        local nvtsi = require("nvim-treesitter.install")
+        nvtsi.update({ with_sync = true })()
+        nvtsi.compilers = { "cl" }
     end,
     config = function()
         require'nvim-treesitter.configs'.setup {
             ensure_installed = { "c", "lua", "vim", "rust", "tsx", "sql"},
-            auto_install = true,
+            auto_install = false, -- while compiler only works in x64 tools
             highlight = {
                 enable = true,
             }
